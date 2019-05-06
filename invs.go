95cdd2f484a6977b2441b37d3d89fa5d27cf1acb
kozakana <goki727@gmail.com>
2019-05-07 00:20:17 +0900

    Add port option specification

--- invs.go
+++ invs.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
 )
+
+type options struct {
+	port string
+}
+
+func (opts options) addr() string {
+	return fmt.Sprintf(":%s", opts.port)
+}
+
+func getOptions() options {
+	opts := options{}
+	flag.StringVar(&opts.port, "p", "8080", "port")
+	flag.Parse()
+	return opts
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "200")
@@ -13,6 +29,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	opts := getOptions()
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("server started on port %s\n", opts.port)
+	http.ListenAndServe(opts.addr(), nil)
 }
