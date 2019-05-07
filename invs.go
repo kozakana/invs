3b33fc234a90203586ec44af6c0f4deb9e0e4f01
kozakana <goki727@gmail.com>
2019-05-07 23:48:56 +0900

    Add filter-url option

--- invs.go
+++ invs.go
@@ -5,31 +5,37 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 )
 
 type options struct {
-	port string
+	port      string
+	filterUrl string
 }
+
+var opts = options{}
 
 func (opts options) addr() string {
 	return fmt.Sprintf(":%s", opts.port)
 }
 
-func getOptions() options {
-	opts := options{}
+func setOptions() {
 	flag.StringVar(&opts.port, "p", "8080", "port")
+	flag.StringVar(&opts.filterUrl, "filter-url", "", "Output only URLs that contain the string")
 	flag.Parse()
-	return opts
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "200")
+	if !strings.Contains(r.URL.Path, opts.filterUrl) {
+		return
+	}
 	dump, _ := httputil.DumpRequest(r, true)
 	fmt.Println(string(dump))
 }
 
 func main() {
-	opts := getOptions()
+	setOptions()
 	http.HandleFunc("/", handler)
 	fmt.Printf("server started on port %s\n", opts.port)
 	http.ListenAndServe(opts.addr(), nil)
