61098a86354ef38ad7c7d754a273a60eb10c1a9c
kozakana <goki727@gmail.com>
2019-05-20 00:02:30 +0900

    add proxy function

--- invs.go
+++ invs.go
@@ -9,7 +9,7 @@ import (
 )
 
 type options struct {
-	port, filterURL string
+	port, filterURL, proxyHost string
 }
 
 var opts = options{}
@@ -20,22 +20,59 @@ func (opts options) addr() string {
 
 func setOptions() {
 	flag.StringVar(&opts.port, "p", "8080", "port")
+	flag.StringVar(&opts.proxyHost, "proxy-host", "", "port")
 	flag.StringVar(&opts.filterURL, "filter-url", "", "Output only URLs that contain the string")
 	flag.Parse()
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(w, "200")
-	if !strings.Contains(r.URL.Path, opts.filterURL) {
+	if !strings.Contains(request.URL.Path, opts.filterURL) {
 		return
 	}
-	dump, _ := httputil.DumpRequest(r, true)
+	dump, _ := httputil.DumpRequest(request, true)
 	fmt.Println(string(dump))
+}
+
+func handler() *http.ServeMux {
+	m := http.NewServeMux()
+	m.Handle("/", http.HandlerFunc(index))
+	return m
+}
+
+func rpHandler() *httputil.ReverseProxy {
+	director := func(request *http.Request) {
+		request.URL.Scheme = "http"
+		request.URL.Host = opts.proxyHost
+		if strings.Contains(request.URL.Path, opts.filterURL) {
+			dump, _ := httputil.DumpRequest(request, true)
+			fmt.Println(string(dump))
+		}
+	}
+	return &httputil.ReverseProxy{Director: director}
 }
 
 func main() {
 	setOptions()
-	http.HandleFunc("/", handler)
-	fmt.Printf("server started on port %s\n", opts.port)
-	http.ListenAndServe(opts.addr(), nil)
+	var server http.Server
+	if opts.proxyHost == "" {
+		server = http.Server{
+			Addr:    opts.addr(),
+			Handler: handler(),
+		}
+	} else {
+		server = http.Server{
+			Addr:    opts.addr(),
+			Handler: rpHandler(),
+		}
+	}
+
+	fmt.Printf("server starting on port %s\n", opts.port)
+	if opts.proxyHost != "" {
+		fmt.Printf("proxy to %s\n", opts.proxyHost)
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Println(err.Error())
+	}
 }
